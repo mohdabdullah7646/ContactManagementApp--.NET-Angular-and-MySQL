c24729bb6a6b06b0095e44c076f987dbde3b72f9
Mohd Abdullah <mohdabdullah7646@gmail.com>
2024-07-11 09:04:33 +0530

    Initial commit

---
+++ crud-angular/src/app/token-http-interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const tokenHttpInterceptor: HttpInterceptorFn = (req, next) => {
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    console.log('tokenHttpInterceptor', token);
+
+    if (token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + token,
+        },
+      });
+    }
+  }
+  
+  return next(req);
+};
